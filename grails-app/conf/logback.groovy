013eebab8ca957d04360f1b08ddcaf70e25238f8
Fylyheng <fylyheng.phally@gmail.com>
2020-06-04 15:02:34 +0700

    update config Sentry tracking Issues

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -35,27 +35,48 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+}
+def dateFile = new Date().format('yyyy-MM-dd')
+def LOG_PATH = "logs" // path to write log
+def LOG_ARCHIVE = "${LOG_PATH}/archive" //path to append to zip
 
-    def dateFile = new Date().format('yyyy-MM-dd')
-    def LOG_PATH = "logs" // path to write log
-    def LOG_ARCHIVE = "${LOG_PATH}/archive" //path to append to zip
 
-    appender("ROLLING-ERROR",RollingFileAppender){
-        file = "${LOG_PATH}/errorFile.${dateFile}.log"
-        encoder(PatternLayoutEncoder) {
-            pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS a} %-6p [%t] %c :%L ====== %m%n"
-        }
-        rollingPolicy(TimeBasedRollingPolicy) {
-            fileNamePattern = "${LOG_ARCHIVE}/errorFile.%d{yyyy-MM-dd-HH}.zip" //property defines a file name pattern for archived log files
-            maxHistory = 30
-            totalSizeCap = FileSize.valueOf("10MB") //Oldest archives are deleted asynchronously when the total size cap is exceeded
-        }
-        filter(ThresholdFilter){
-            level = INFO
-        }
+// write logger file for error file
+appender("ROLLING-ERROR", RollingFileAppender) {
+    file = "${LOG_PATH}/errorFile.${dateFile}.log"
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS a} %-6p [%t] %c :%L ====== %m%n"
     }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${LOG_ARCHIVE}/errorFile.%d{yyyy-MM-dd-HH}.zip"
+        //property defines a file name pattern for archived log files
+        maxHistory = 30
+        totalSizeCap = FileSize.valueOf("10MB")
+        //Oldest archives are deleted asynchronously when the total size cap is exceeded
+    }
+    filter(ThresholdFilter) {
+        level = INFO
+    }
+}
 
-    //DB appender write to database
+
+// write logger file for all or info file
+appender("ROLLING-INFO", RollingFileAppender) {
+    file = "${LOG_PATH}/infoFile.${dateFile}.log"
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{yyyy-MM-dd HH:mm:ss.SSS a} %-6p [%t] %c :%L ====== %m%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "${LOG_ARCHIVE}/infoFile.%d{yyyy-MM-dd-HH}.zip"
+        maxHistory = 30
+        totalSizeCap = FileSize.valueOf("100MB")
+    }
+    filter(ThresholdFilter){
+        level = INFO // set log level
+    }
+}
+
+//DB appender write to database
 //appender("DB-INFO", DBAppender){
 //    connectionSource(DriverManagerConnectionSource){
 //        driverClass = "com.mysql.jdbc.Driver"
@@ -66,22 +87,19 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
 //}
 //AsyncAppender It acts solely as an event dispatcher and must therefore reference another appender in order to do anything useful
 // appender ref to file name rolling error
-//    appender("ERROR", AsyncAppender) {
-//        appenderRef("ROLLING-ERROR")
-//    }
+    appender("ERROR", AsyncAppender) {
+        appenderRef("ROLLING-ERROR")
+    }
 // appender ref to file name rolling info
-//    appender("INFO", AsyncAppender) {
-//        appenderRef("ROLLING-INFO")
-//    }
+    appender("INFO", AsyncAppender) {
+        appenderRef("ROLLING-INFO")
+    }
 // appender ref to file name db info
 //appender("DB", AsyncAppender) {
 //    appenderRef("DB-INFO")
 //}
-
-
-}
 //logger("corebackend", INFO, ["STDOUT"], true)
 //logger("org.springframework.security", DEBUG, ['STDOUT'], false)
 //logger("grails.plugin.springsecurity", DEBUG, ['STDOUT'], false)
 
-root(ERROR, ['STDOUT'])
+root(INFO, ['STDOUT', 'ERROR', 'INFO'])
